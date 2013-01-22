c0f89490dd344a57977769dbd2165573ed6459a6
Duncan Mason <duncan.mason@huzutech.com>
2013-01-22 09:10:55 +0000

    fuck knows

--- specs/duncan-wd.js
+++ specs/duncan-wd.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 var driver = webdriver.remote();
 
 driver.init(function(sessionId, err){
-	testHapiCMS();
+	testHapiCMS(driver);
 });
 
 function testGoogle(driver){
@@ -33,7 +33,6 @@ function testGoogle(driver){
 
 }
 
-<<<<<<< HEAD
 function setElementValue(selector, value, cb)
 {
 	driver.elementByCss(selector, function(err, element){
@@ -44,61 +43,36 @@ function setElementValue(selector, value, cb)
 	});
 }
 
-function testHapiCMS(){
-	driver.get('https://hapi-cms-test.herokuapp.com/?whatthefuckisthis', function(err, url){
-			driver.elementByCss(
-				"div.hzt-app-content iframe", function(err, element){
-				console.log("element toString "+element.toString());
-				console.log(arguments);
-
-console.log(element);
-
-				driver.getTagName(element, function(err, name){
-=======
 function testHapiCMS(browser){
 	browser.get('https://hapi-cms-test.herokuapp.com/', function(err, url){
-			
+		browser.elementByTagName("iframe", function(err, element){
 
-			browser.elementByCss("footer", function(err, element){
 if(err)console.log("err = "+err);
 console.log(element);	
-console.log(element["ELEMENT"]);
-return;
-				browser.getTagName(element, function(err, name){
->>>>>>> 728f2f71fea364e1856df1824081a561bcb222bd
+
+			browser.getTagName(element, function(err, name){
 
 console.log('\ntag name:'+name);
 
-					driver.frame(Number(element.toString()), function(err){
-							if(err) console.log('error: '+err);
-<<<<<<< HEAD
-					     
-						setElementValue("#Username", "owner", function(){
-							setElementValue("#Password", "HuzuRocks!", function(){
-								driver.elementByCss("#login-btn", function(err, element){
-									element.click(function (err) {
-										driver.frame(null, function(err){
-											driver.waitForVisibleByCss("div.home", 2000, function(err){
-												assert.ifError(err);
-												driver.quit();
-											});
+				driver.frame(Number(element.toString()), function(err){
+					if(err) console.log('error: '+err);
+				     
+					setElementValue("#Username", "owner", function(){
+						setElementValue("#Password", "HuzuRocks!", function(){
+							driver.elementByCss("#login-btn", function(err, element){
+								element.click(function (err) {
+									driver.frame(null, function(err){
+										driver.waitForVisibleByCss("div.home", 2000, function(err){
+											assert.ifError(err);
+											driver.quit();
 										});
 									});
-							
 								});
-=======
-							browser.title(function(err, title){
-
-console.log('\ntitle:'+title);
->>>>>>> 728f2f71fea364e1856df1824081a561bcb222bd
 							});
-						});
-							
-							
+						});	
 					});
-
 				});
-				driver.text(element, function(err, text){ console.log(text);})
 			});
-	})
+		});
+	});
 }
